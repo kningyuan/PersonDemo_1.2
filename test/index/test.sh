77066244cf2267b57332a14c94ccd022850d11ae
kningyuan <gezhongluntan163@163.com>
2018-08-21 21:55:42 -0700

    delete file

--- test/index/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-./testAPIs_chancode.sh index_register 341221199202235792 
-./testAPIs_chancode.sh index_register 341221199202235792 
-./testAPIs_chancode.sh index_register 341221199202235792 
-./testAPIs_chancode.sh index_query 341221199202235792
--- test/interface/instance.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-../authority/testAPIs_instance.sh
-../xuexin/testAPIs_instance.sh
-../index/testAPIs_instance.sh
-./testAPIs_instance.sh
